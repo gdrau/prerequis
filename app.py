2c04efe716d90fe5fc0dcbaad3c4f1a249e7c9fa
gdrau <33457432+gdrau@users.noreply.github.com>
2018-04-12 17:39:44 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ project_id = 'B85DD89411E83A9413360080EF15F2B2'
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
+    console.log("coucou");
     print("Request:")
     print(json.dumps(req, indent=4))
     res = processRequest(req)
