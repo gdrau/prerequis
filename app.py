cc355785b8b0c71167c86458aa7810556af77eb6
gdrau <33457432+gdrau@users.noreply.github.com>
2018-04-13 17:01:22 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def login():
     #print("Obtention token...")
     data_get = { "username": "administrator",
                  "password": ""}
-    #r = requests.post(base_url + 'auth/login', data=data_get)
-    #authToken = r.headers['X-MSTR-AuthToken']
+    r = requests.post(base_url + 'auth/login', data=data_get)
+    authToken = r.headers['X-MSTR-AuthToken']
     toto="speech"
     return toto
 
