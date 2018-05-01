06070138b9bd40bb2bc600a4084e380a5ee8be43
gdrau <33457432+gdrau@users.noreply.github.com>
2018-05-01 23:45:48 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python
-#-*- coding:utf8 -*-
+# -*- coding:utf8 -*-
 # Copyright 2017 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,15 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import urllib2
-import requests
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+
 import base64
 import json
 
 from pprint import pprint
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
+
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
@@ -37,60 +38,63 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
-
-
 
 ### Parametre MicroStrategy ###
 api_login = 'administrator'
 api_password = ''
-api_iserver = 'mon.prerequis.com:2051'
+api_iserver = '192.168.1.96'
 project_id = 'B85DD89411E83A9413360080EF15F2B2'
 base_url = "http://mon.prerequis.com:2051/MicroStrategyLibrary/api/";
 
 
 
 @app.route('/webhook', methods=['POST'])
-
-
 def webhook():
     req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
     res = processRequest(req)
     res = json.dumps(res, indent=4)
-    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 #### Recuperation du token MicroStrategy ###
 def login():
-    #print("Obtention token...")
-    r = requests.post("https://query.yahooapis.com")
+   #print("Obtention token...")
+    data_get = { "username": "administrator",
+                 "password": "",
+                 "loginMode": "1",
+                 "maxSearch": "3",
+                 "workingSet": 0,
+                 "changePassword": "false",
+                 "newPassword": "string",
+                 "metadataLocale": "en_us",
+                 "warehouseDataLocale": "en_us",
+                 "displayLocale": "en_us",
+                 "messagesLocale": "en_us",
+                 "numberLocale": "en_us",
+                 "timeZone": "UTC",
+                 "applicationType": "35" }
+    r = requests.post(base_url + 'auth/login', data=data_get)
     if r.ok:
-        tt="toto"
+	authToken = r.headers['X-MSTR-AuthToken']
+        cookies = dict(r.cookies)
         #print("Token: " + authToken)
-        return tt
+        return authToken, cookies
     else:
         print("HTTP %i - %s, Message %s" % (r.status_code, r.reason, r.text))
 
+                    
+                    
 def processRequest(req):
     if req.get("result").get("action") != "congessalarie":
         return {}
-    a=login()
-    res = makeWebhookResult()
-    return res
-
-
-
+    authToken = login()
+    res=makeWebhookResult()
+    return res   
 
 def makeWebhookResult():
     
-    
-    speech = "allo"
+    speech = "coucou ca va?"  
     
     print("Response:")
     print(speech)
