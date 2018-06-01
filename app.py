c7a708e4bf3c339b6e16dc5cd375bfc60928d63f
gdrau <33457432+gdrau@users.noreply.github.com>
2018-06-01 17:45:01 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -134,6 +134,7 @@ def makeWebhookResult(datastore):
         "source": "apigdr-webhook"
     }
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.debug = True
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
